0f0915b98c9db6102fa98c176b3a950ff074231a
David Lenaerts <david.lenaerts@gmail.com>
2015-05-14 03:44:41 +0200

    Sync scrubbing to frame beat

--- lib/display/MovieClip.ts
+++ lib/display/MovieClip.ts
@@ -22,6 +22,7 @@ class MovieClip extends DisplayObjectContainer
     private _keyFrameActive:Array<number>;
     private _time:number;// the current time inside the animation
     private _currentFrameIndex:number;// the current frame
+    private _jumpIndex:number;// the current frame
     private _currentKeyFrameIndex:number;// the current index of the current active frame
     private _fps:number;// we use ms internally, but have fps, so user can set time by frame
     private _isPlaying:boolean;// false if paused or stopped
@@ -30,7 +31,6 @@ class MovieClip extends DisplayObjectContainer
     // not sure if needed
     private _prototype:MovieClip;
     private _enterFrame:MovieClipEvent;
-    private _skipAdvance : boolean;
 
     private _adapter:MovieClipAdapter;
 
@@ -46,6 +46,7 @@ class MovieClip extends DisplayObjectContainer
         this._potentialPrototypes = [];
         this._potentialInstances = [];
         this._currentFrameIndex = -1;
+        this._jumpIndex = -1;
         this._currentKeyFrameIndex = -1;
         this._isPlaying = true; // auto-play
         this._fps = 30;
@@ -80,30 +81,19 @@ class MovieClip extends DisplayObjectContainer
 
     public get currentFrameIndex() : number
     {
-        return this._currentFrameIndex;
+        return this._jumpIndex >= 0? this._jumpIndex : this._currentFrameIndex;
     }
 
     public set currentFrameIndex(value : number)
     {
         value = Math.floor(value);
+
         if (value < 0)
             value = 0;
         else if (value >= this._numFrames)
             value = this._numFrames - 1;
 
-        this._time = 0;
-
-        var isPlaying = this._isPlaying;
-        this._isPlaying = true;
-
-        while (this._currentFrameIndex != value) {
-            // do not advance children, do not call post-constructs (scripts etc, only constructs are relevant)
-            this.advanceFrame(true);
-        }
-
-        this._skipAdvance = true;
-
-        this._isPlaying = isPlaying;
+        this._jumpIndex = value;
     }
 
     // adapter is used to provide MovieClip to scripts taken from different platforms
@@ -195,9 +185,24 @@ class MovieClip extends DisplayObjectContainer
         if (this._time > frameMarker) {
             this._time = 0;
             this.advanceFrame();
+            this.advanceChildren();
             this.dispatchEvent(this._enterFrame);
             this.executePostConstructCommands();
         }
+    }
+
+    private _jumpToIndex()
+    {
+        var isPlaying = this._isPlaying;
+        this._isPlaying = true;
+
+        while (this._currentFrameIndex != this._jumpIndex) {
+            // do not advance children, do not call post-constructs (scripts etc, only constructs are relevant)
+            this.advanceFrame(true);
+        }
+
+        this._isPlaying = isPlaying;
+        this._jumpIndex = -1;
     }
 
     /**
@@ -325,35 +330,42 @@ class MovieClip extends DisplayObjectContainer
 
     private advanceFrame(skipChildren:boolean = false)
     {
-        var i;
-        var oldFrameIndex = this._currentFrameIndex;
-        var advance = this._isPlaying && !this._skipAdvance;
-        if (advance && this._currentFrameIndex == this._numFrames - 1 && !this._loop) {
-            advance = false;
+        if (!skipChildren && this._jumpIndex !== -1) {
+            this._jumpToIndex();
         }
-        if (advance && this._currentFrameIndex <= 0 && this._numFrames == 1) {
-            this._currentFrameIndex = 0;
-            advance = false;
+        else {
+            var oldFrameIndex = this._currentFrameIndex;
+            var i;
+            var advance = this._isPlaying;
+            if (advance && this._currentFrameIndex == this._numFrames - 1 && !this._loop) {
+                advance = false;
+            }
+            if (advance && this._currentFrameIndex <= 0 && this._numFrames == 1) {
+                this._currentFrameIndex = 0;
+                advance = false;
+            }
+
+
+            if (advance) {
+                if (++this._currentFrameIndex == this._numFrames)
+                    this.resetPlayHead();
+            }
+
+            if (oldFrameIndex != this._currentFrameIndex)
+                this.updateKeyFrames(skipChildren);
         }
+    }
 
-        if (advance) {
-            if (++this._currentFrameIndex == this._numFrames)
-                this.resetPlayHead();
-        }
-
-        if (oldFrameIndex != this._currentFrameIndex || this._skipAdvance)
-            this.updateKeyFrames(skipChildren);
-
-        if (!skipChildren) {
-            var len = this.numChildren;
-            for (i = 0; i <  len; ++i) {
-                var child = this.getChildAt(i);
-                if (child instanceof MovieClip)
-                    (<MovieClip>child).advanceFrame();
+    private advanceChildren()
+    {
+        var len = this.numChildren;
+        for (var i = 0; i <  len; ++i) {
+            var child = this.getChildAt(i);
+            if (child instanceof MovieClip) {
+                (<MovieClip>child).advanceFrame();
+                (<MovieClip>child).advanceChildren();
             }
         }
-
-        this._skipAdvance = false;
     }
 
     private updateKeyFrames(skipFrames:boolean)
