2b2b3ab3ba54d7f742469c86f03ec74e9aa73abd
rob-bateman <rob@infiniteturtles.co.uk>
2015-06-07 03:47:27 +0100

    update build files

--- build/awayjs-player.d.ts
+++ build/awayjs-player.d.ts
@@ -4,6 +4,97 @@ declare module "awayjs-player/lib/adapters/AS2ColorAdapter" {
 	
 }
 
+declare module "awayjs-player/lib/adapters/AS2KeyAdapter" {
+	class AS2KeyAdapter {
+	    private static _keys;
+	    private static _key;
+	    private static _char;
+	    static _listeners: Array<any>;
+	    private static _addListeners;
+	    static addListener(listener: any): void;
+	    static removeListener(listener: any): void;
+	    static isDown(code: number): boolean;
+	    static getCode(): number;
+	    static getAscii(): number;
+	    /**
+	     * Constant associated with the key code value for the Backspace key (8).
+	     */
+	    static BACKSPACE: number;
+	    /**
+	     * Constant associated with the key code value for the Caps Lock key (20).
+	     */
+	    static CAPSLOCK: number;
+	    /**
+	     * Constant associated with the key code value for the Control key (17).
+	     */
+	    static CONTROL: number;
+	    /**
+	     * Constant associated with the key code value for the Delete key (46).
+	     */
+	    static DELETEKEY: number;
+	    /**
+	     * Constant associated with the key code value for the Down Arrow key (40).
+	     */
+	    static DOWN: number;
+	    /**
+	     * Constant associated with the key code value for the End key (35).
+	     */
+	    static END: number;
+	    /**
+	     * Constant associated with the key code value for the Enter key (13).
+	     */
+	    static ENTER: number;
+	    /**
+	     * Constant associated with the key code value for the Escape key (27).
+	     */
+	    static ESCAPE: number;
+	    /**
+	     * Constant associated with the key code value for the Home key (36).
+	     */
+	    static HOME: number;
+	    /**
+	     * Constant associated with the key code value for the Insert key (45).
+	     */
+	    static INSERT: number;
+	    /**
+	     * Constant associated with the key code value for the Left Arrow key (37).
+	     */
+	    static LEFT: number;
+	    /**
+	     * Constant associated with the key code value for the Page Down key (34).
+	     */
+	    static PGDN: number;
+	    /**
+	     * Constant associated with the key code value for the Page Up key (33).
+	     */
+	    static PGUP: number;
+	    /**
+	     * Constant associated with the key code value for the Right Arrow key (39).
+	     */
+	    static RIGHT: number;
+	    /**
+	     * Constant associated with the key code value for the Shift key (16).
+	     */
+	    static SHIFT: number;
+	    /**
+	     * Constant associated with the key code value for the Spacebar (32).
+	     */
+	    static SPACE: number;
+	    /**
+	     * Constant associated with the key code value for the Tab key (9).
+	     */
+	    static TAB: number;
+	    /**
+	     * Constant associated with the key code value for the Up Arrow key (38).
+	     */
+	    static UP: number;
+	    private static _onKeyDown(event);
+	    private static _onKeyUp(event);
+	}
+	export = AS2KeyAdapter;
+	
+}
+
 declare module "awayjs-player/lib/adapters/AS2MCSoundProps" {
 	import EventDispatcher = require("awayjs-core/lib/events/EventDispatcher");
 	class AS2MCSoundProps extends EventDispatcher {
@@ -84,17 +175,6 @@ declare module "awayjs-player/lib/adapters/AS2MovieClipAdapter" {
 	
 }
 
-declare module "awayjs-player/lib/adapters/AS2SharedObjectAdapter" {
-	class AS2SharedObjectAdapter {
-	    data: Object;
-	    constructor();
-	    static getLocal(name: string, localPath: string, secure: boolean): AS2SharedObjectAdapter;
-	    flush(): void;
-	}
-	export = AS2SharedObjectAdapter;
-	
-}
-
 declare module "awayjs-player/lib/adapters/AS2SoundAdapter" {
 	import AS2MovieClipAdapter = require("awayjs-player/lib/adapters/AS2MovieClipAdapter");
 	class AS2SoundAdapter {
@@ -121,6 +201,17 @@ declare module "awayjs-player/lib/adapters/AS2SoundAdapter" {
 	
 }
 
+declare module "awayjs-player/lib/adapters/AS2SharedObjectAdapter" {
+	class AS2SharedObjectAdapter {
+	    data: Object;
+	    constructor();
+	    static getLocal(name: string, localPath: string, secure: boolean): AS2SharedObjectAdapter;
+	    flush(): void;
+	}
+	export = AS2SharedObjectAdapter;
+	
+}
+
 declare module "awayjs-player/lib/adapters/AS2StageAdapter" {
 	class AS2StageAdapter {
 	    static showMenu: boolean;
@@ -133,9 +224,11 @@ declare module "awayjs-player/lib/adapters/AS2SymbolAdapter" {
 	import DisplayObjectContainer = require("awayjs-display/lib/containers/DisplayObjectContainer");
 	import AS2SharedObjectAdapter = require("awayjs-player/lib/adapters/AS2SharedObjectAdapter");
 	import AS2MovieClipAdapter = require("awayjs-player/lib/adapters/AS2MovieClipAdapter");
+	import AS2KeyAdapter = require("awayjs-player/lib/adapters/AS2KeyAdapter");
 	import AS2MouseAdapter = require("awayjs-player/lib/adapters/AS2MouseAdapter");
 	import AS2StageAdapter = require("awayjs-player/lib/adapters/AS2StageAdapter");
 	class AS2SymbolAdapter {
+	    Key: typeof AS2KeyAdapter;
 	    Mouse: typeof AS2MouseAdapter;
 	    Stage: typeof AS2StageAdapter;
 	    SharedObject: typeof AS2SharedObjectAdapter;
@@ -176,15 +269,6 @@ declare module "awayjs-player/lib/adapters/AS2SymbolAdapter" {
 	
 }
 
-declare module "awayjs-player/lib/adapters/AS2SystemAdapter" {
-	class AS2SystemAdapter {
-	    static security: Object;
-	    static capabilities: Object;
-	}
-	export = AS2SystemAdapter;
-	
-}
-
 declare module "awayjs-player/lib/adapters/AS2TextFieldAdapter" {
 	import AS2SymbolAdapter = require("awayjs-player/lib/adapters/AS2SymbolAdapter");
 	import TextFieldAdapter = require("awayjs-player/lib/adapters/TextFieldAdapter");
@@ -197,6 +281,15 @@ declare module "awayjs-player/lib/adapters/AS2TextFieldAdapter" {
 	    text: string;
 	}
 	export = AS2TextFieldAdapter;
+	
+}
+
+declare module "awayjs-player/lib/adapters/AS2SystemAdapter" {
+	class AS2SystemAdapter {
+	    static security: Object;
+	    static capabilities: Object;
+	}
+	export = AS2SystemAdapter;
 	
 }
 
@@ -325,18 +418,6 @@ declare module "awayjs-player/lib/events/MovieClipEvent" {
 	
 }
 
-declare module "awayjs-player/lib/factories/AS2SceneGraphFactory" {
-	import MovieClip = require("awayjs-player/lib/display/MovieClip");
-	import AdaptedTextField = require("awayjs-player/lib/display/AdaptedTextField");
-	import TimelineSceneGraphFactory = require("awayjs-player/lib/factories/TimelineSceneGraphFactory");
-	class AS2SceneGraphFactory implements TimelineSceneGraphFactory {
-	    createMovieClip(): MovieClip;
-	    createTextField(): AdaptedTextField;
-	}
-	export = AS2SceneGraphFactory;
-	
-}
-
 declare module "awayjs-player/lib/factories/TimelineSceneGraphFactory" {
 	import MovieClip = require("awayjs-player/lib/display/MovieClip");
 	import TextField = require("awayjs-display/lib/entities/TextField");
@@ -355,6 +436,18 @@ declare module "awayjs-player/lib/partition/Partition2D" {
 	    constructor(root: DisplayObject);
 	}
 	export = Partition2D;
+	
+}
+
+declare module "awayjs-player/lib/factories/AS2SceneGraphFactory" {
+	import MovieClip = require("awayjs-player/lib/display/MovieClip");
+	import AdaptedTextField = require("awayjs-player/lib/display/AdaptedTextField");
+	import TimelineSceneGraphFactory = require("awayjs-player/lib/factories/TimelineSceneGraphFactory");
+	class AS2SceneGraphFactory implements TimelineSceneGraphFactory {
+	    createMovieClip(): MovieClip;
+	    createTextField(): AdaptedTextField;
+	}
+	export = AS2SceneGraphFactory;
 	
 }
 
--- build/awayjs-player.js
+++ build/awayjs-player.js
@@ -36,7 +36,125 @@ var AS2ColorAdapter = (function () {
 })();
 module.exports = AS2SymbolAdapter;
 
-},{"awayjs-player/lib/adapters/AS2SymbolAdapter":"awayjs-player/lib/adapters/AS2SymbolAdapter"}],"awayjs-player/lib/adapters/AS2MCSoundProps":[function(require,module,exports){
+},{"awayjs-player/lib/adapters/AS2SymbolAdapter":"awayjs-player/lib/adapters/AS2SymbolAdapter"}],"awayjs-player/lib/adapters/AS2KeyAdapter":[function(require,module,exports){
+var AS2KeyAdapter = (function () {
+    function AS2KeyAdapter() {
+    }
+    AS2KeyAdapter.addListener = function (listener) {
+        AS2KeyAdapter._listeners.push(listener);
+    };
+    AS2KeyAdapter.removeListener = function (listener) {
+        var listeners = AS2KeyAdapter._listeners;
+        var index = listeners.indexOf(listener);
+        if (index != -1)
+            listeners.splice(index, 1);
+    };
+    AS2KeyAdapter.isDown = function (code) {
+        return AS2KeyAdapter._keys[code];
+    };
+    AS2KeyAdapter.getCode = function () {
+        return AS2KeyAdapter._key;
+    };
+    AS2KeyAdapter.getAscii = function () {
+        return AS2KeyAdapter._char;
+    };
+    AS2KeyAdapter._onKeyDown = function (event) {
+        AS2KeyAdapter._key = event.keyCode;
+        AS2KeyAdapter._char = event.charCode;
+        AS2KeyAdapter._keys[event.keyCode] = true;
+        var len = AS2KeyAdapter._listeners.length;
+        for (var i = 0; i < len; i++)
+            AS2KeyAdapter._listeners[i].onKeyDown();
+    };
+    AS2KeyAdapter._onKeyUp = function (event) {
+        AS2KeyAdapter._keys[event.keyCode] = false;
+        var len = AS2KeyAdapter._listeners.length;
+        for (var i = 0; i < len; i++)
+            AS2KeyAdapter._listeners[i].onKeyUp();
+    };
+    AS2KeyAdapter._keys = new Array();
+    AS2KeyAdapter._listeners = new Array();
+    AS2KeyAdapter._addListeners = (function () {
+        document.onkeydown = function (event) { return AS2KeyAdapter._onKeyDown(event); };
+        document.onkeyup = function (event) { return AS2KeyAdapter._onKeyUp(event); };
+    })();
+    /**
+     * Constant associated with the key code value for the Backspace key (8).
+     */
+    AS2KeyAdapter.BACKSPACE = 8;
+    /**
+     * Constant associated with the key code value for the Caps Lock key (20).
+     */
+    AS2KeyAdapter.CAPSLOCK = 20;
+    /**
+     * Constant associated with the key code value for the Control key (17).
+     */
+    AS2KeyAdapter.CONTROL = 17;
+    /**
+     * Constant associated with the key code value for the Delete key (46).
+     */
+    AS2KeyAdapter.DELETEKEY = 46;
+    /**
+     * Constant associated with the key code value for the Down Arrow key (40).
+     */
+    AS2KeyAdapter.DOWN = 40;
+    /**
+     * Constant associated with the key code value for the End key (35).
+     */
+    AS2KeyAdapter.END = 35;
+    /**
+     * Constant associated with the key code value for the Enter key (13).
+     */
+    AS2KeyAdapter.ENTER = 13;
+    /**
+     * Constant associated with the key code value for the Escape key (27).
+     */
+    AS2KeyAdapter.ESCAPE = 27;
+    /**
+     * Constant associated with the key code value for the Home key (36).
+     */
+    AS2KeyAdapter.HOME = 36;
+    /**
+     * Constant associated with the key code value for the Insert key (45).
+     */
+    AS2KeyAdapter.INSERT = 45;
+    /**
+     * Constant associated with the key code value for the Left Arrow key (37).
+     */
+    AS2KeyAdapter.LEFT = 37;
+    /**
+     * Constant associated with the key code value for the Page Down key (34).
+     */
+    AS2KeyAdapter.PGDN = 34;
+    /**
+     * Constant associated with the key code value for the Page Up key (33).
+     */
+    AS2KeyAdapter.PGUP = 33;
+    /**
+     * Constant associated with the key code value for the Right Arrow key (39).
+     */
+    AS2KeyAdapter.RIGHT = 39;
+    /**
+     * Constant associated with the key code value for the Shift key (16).
+     */
+    AS2KeyAdapter.SHIFT = 16;
+    /**
+     * Constant associated with the key code value for the Spacebar (32).
+     */
+    AS2KeyAdapter.SPACE = 32;
+    /**
+     * Constant associated with the key code value for the Tab key (9).
+     */
+    AS2KeyAdapter.TAB = 9;
+    /**
+     * Constant associated with the key code value for the Up Arrow key (38).
+     */
+    AS2KeyAdapter.UP = 38;
+    return AS2KeyAdapter;
+})();
+module.exports = AS2KeyAdapter;
+
+},{}],"awayjs-player/lib/adapters/AS2MCSoundProps":[function(require,module,exports){
 var __extends = this.__extends || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
@@ -568,7 +686,9 @@ var AS2StageAdapter = (function () {
 module.exports = AS2StageAdapter;
 
 },{}],"awayjs-player/lib/adapters/AS2SymbolAdapter":[function(require,module,exports){
+var ColorTransform = require("awayjs-core/lib/geom/ColorTransform");
 var AS2SharedObjectAdapter = require("awayjs-player/lib/adapters/AS2SharedObjectAdapter");
+var AS2KeyAdapter = require("awayjs-player/lib/adapters/AS2KeyAdapter");
 var AS2MouseAdapter = require("awayjs-player/lib/adapters/AS2MouseAdapter");
 var AS2StageAdapter = require("awayjs-player/lib/adapters/AS2StageAdapter");
 // also contains global AS2 gunctions
@@ -585,9 +705,16 @@ var AS2SymbolAdapter = (function () {
             AS2SymbolAdapter.CLASS_REPLACEMENTS["Sound"] = "awayjs-player/lib/adapters/AS2SoundAdapter";
         }
     }
-    Object.defineProperty(AS2SymbolAdapter.prototype, "Mouse", {
+    Object.defineProperty(AS2SymbolAdapter.prototype, "Key", {
         // TODO: REMOVE AND PROVIDE AS CLASS (See System) ONCE TRANSLATOR IS FIXED
         // And then change properties to statics
+        get: function () {
+            return AS2KeyAdapter;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AS2SymbolAdapter.prototype, "Mouse", {
         get: function () {
             return AS2MouseAdapter;
         },
@@ -718,6 +845,8 @@ var AS2SymbolAdapter = (function () {
             return this.adaptee.transform.colorTransform.alphaMultiplier;
         },
         set: function (value) {
+            if (!this.adaptee.transform.colorTransform)
+                this.adaptee.transform.colorTransform = new ColorTransform();
             this.adaptee.transform.colorTransform.alphaMultiplier = value;
         },
         enumerable: true,
@@ -793,7 +922,7 @@ var AS2SymbolAdapter = (function () {
 })();
 module.exports = AS2SymbolAdapter;
 
-},{"awayjs-player/lib/adapters/AS2MouseAdapter":"awayjs-player/lib/adapters/AS2MouseAdapter","awayjs-player/lib/adapters/AS2SharedObjectAdapter":"awayjs-player/lib/adapters/AS2SharedObjectAdapter","awayjs-player/lib/adapters/AS2StageAdapter":"awayjs-player/lib/adapters/AS2StageAdapter"}],"awayjs-player/lib/adapters/AS2SystemAdapter":[function(require,module,exports){
+},{"awayjs-core/lib/geom/ColorTransform":undefined,"awayjs-player/lib/adapters/AS2KeyAdapter":"awayjs-player/lib/adapters/AS2KeyAdapter","awayjs-player/lib/adapters/AS2MouseAdapter":"awayjs-player/lib/adapters/AS2MouseAdapter","awayjs-player/lib/adapters/AS2SharedObjectAdapter":"awayjs-player/lib/adapters/AS2SharedObjectAdapter","awayjs-player/lib/adapters/AS2StageAdapter":"awayjs-player/lib/adapters/AS2StageAdapter"}],"awayjs-player/lib/adapters/AS2SystemAdapter":[function(require,module,exports){
 // also contains global AS2 functions
 var AS2SystemAdapter = (function () {
     function AS2SystemAdapter() {
