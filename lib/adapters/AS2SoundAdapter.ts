6820b8edcd850429edb452b5883cfbcb2977f510
80prozent <80prozent@differentdesign.de>
2015-09-03 18:40:40 +0200

    fix rounding volume for java audio control

--- lib/adapters/AS2SoundAdapter.ts
+++ lib/adapters/AS2SoundAdapter.ts
@@ -110,7 +110,7 @@ class AS2SoundAdapter
 
         // todo volume hardcoded to 1
         if(typeof mainApplication !== "undefined")
-            mainApplication.startSound(this._name, this._id, Math.round(this._volume*100)/100, this._loop);
+            mainApplication.startSound(this._name, this._id, this._volume, this._loop);
         else if(this._soundProps.audio)
             this._soundProps.audio.play(offsetInSeconds, this._loop);
     }
@@ -161,7 +161,7 @@ class AS2SoundAdapter
             vol=1;
         if(vol<0)
             vol=0;
-
+        vol=Math.round(vol*100)/100;
         if(this._volume == vol)
             return;
 
@@ -169,7 +169,7 @@ class AS2SoundAdapter
 
         if(typeof mainApplication !== "undefined") {
             if (this._playing)
-                mainApplication.updateSound(this._id, Math.round(this._volume*100)/100, this._loop);
+                mainApplication.updateSound(this._id, this._volume, this._loop);
         } else if (this._soundProps.audio)
             this._soundProps.audio.volume = this._volume;
     }
